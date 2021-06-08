a0cce84a24574113597ee6d7400c84fdd856f325
Jordan Leiker <jpleiker@gmail.com>
2021-06-07 21:36:04 -0400

    Added page listing all sensors that redirects to individual measurements pages

--- poster.py
+++ poster.py
@@ -6,19 +6,22 @@ data = {
         'data_id' : 1,
         'type' : 'fake_data',
         'value' : 3.14159,
-        'ctrlr_id' : 266
+        'ctrlr_id' : None,
+        'ctrlr_type' : "BIGD123",
         }
 
 while(True):
 
-    r = requests.post('http://localhost:5000/log_data', json=data)
+    r = requests.post('http://localhost:5000/record_data', json=data)
     if r.status_code != requests.codes.ok:
         print("Error - Exiting" + str(r.status_code))
         exit()
 
-    print(data)
-    return_data = r.content.decode(('UTF-8'))
-    print(return_data)
+    # If we get a response it's to update our data, so do that
+    if r.content:
+        respDict = r.json()
+        data['ctrlr_id'] = respDict['ctrlr_id']
+
     time.sleep(1)
 
     r = requests.post('http://localhost:5000/print_data', json=data)
